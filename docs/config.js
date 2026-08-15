8234a1e5efbe429d82ab98b3fa88e5ea53c90379
sadashivjagdale-jpg <sadashiv.jagdale@gmail.com>
2026-08-15 19:02:05 +0530

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,1 +1,1 @@
-window.JAGSONS_APP_URL = "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE";
+window.JAGSONS_APP_URL = https://script.google.com/macros/s/AKfycby58D7pVxzi1pHlsThtHyxi-RghkIAlT9nB1Z4pYab1ESFomLmGIErjvImEOImOL8gSgg/exec";window.JAGSONS_APP_URL = "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE";
